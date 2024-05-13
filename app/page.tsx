df91b468fa37c4cb9b64aad5a64b5399571e489f
owen-wang-student <owang420@gmail.com>
2024-05-13 12:55:18 -0500

    initialized project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className="max-w-3xl mx-auto md:py-10 h-screen">
+      <div className="h-full border rounded-md">
+        
+      </div>
+    </div>
+  )
+}
